919d10d25e143f25d1030c40c24209357f2808a6
Nyxby2002 <nyxby2002@gmail.com>
2025-08-09 17:35:02 -0600

    Puerta Embarque

--- Aeropuerto/Frontend/FrmPuertaEmbarque.Designer.cs
+++ Aeropuerto/Frontend/FrmPuertaEmbarque.Designer.cs
@@ -28,63 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox6 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            TxbxTitulo = new TextBox();
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
             BtEliminar = new Button();
-            TxbxTitulo = new TextBox();
+            dateTimePicker2 = new DateTimePicker();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
             SuspendLayout();
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(201, 280);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 63;
+            textBox6.Text = "Ubicacion";
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(390, 164);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 61;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(201, 251);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 60;
+            textBox5.Text = "Capacidad";
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(201, 222);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 59;
+            textBox4.Text = "Estado";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(201, 164);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 58;
+            textBox3.Text = "Numero";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(201, 193);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 57;
+            textBox2.Text = "Terminal";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(201, 135);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 56;
+            textBox1.Text = "ID";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // TxbxTitulo
+            // 
+            TxbxTitulo.Location = new Point(331, 59);
+            TxbxTitulo.Name = "TxbxTitulo";
+            TxbxTitulo.Size = new Size(110, 23);
+            TxbxTitulo.TabIndex = 55;
+            TxbxTitulo.Text = "Puerta Embarque";
+            TxbxTitulo.TextAlign = HorizontalAlignment.Center;
             // 
             // button3
             // 
-            button3.Location = new Point(241, 214);
+            button3.Location = new Point(226, 334);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
-            button3.TabIndex = 7;
+            button3.TabIndex = 54;
             button3.Text = "Guardar";
             button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(484, 214);
+            button2.Location = new Point(469, 334);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
-            button2.TabIndex = 6;
+            button2.TabIndex = 53;
             button2.Text = "Buscar";
             button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(322, 214);
+            button1.Location = new Point(307, 334);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
-            button1.TabIndex = 5;
+            button1.TabIndex = 52;
             button1.Text = "Editar";
             button1.UseVisualStyleBackColor = true;
             // 
             // BtEliminar
             // 
-            BtEliminar.Location = new Point(403, 214);
+            BtEliminar.Location = new Point(388, 334);
             BtEliminar.Name = "BtEliminar";
             BtEliminar.Size = new Size(75, 23);
-            BtEliminar.TabIndex = 4;
+            BtEliminar.TabIndex = 51;
             BtEliminar.Text = "Eliminar";
             BtEliminar.UseVisualStyleBackColor = true;
             // 
-            // TxbxTitulo
+            // dateTimePicker2
             // 
-            TxbxTitulo.Location = new Point(352, 165);
-            TxbxTitulo.Name = "TxbxTitulo";
-            TxbxTitulo.Size = new Size(94, 23);
-            TxbxTitulo.TabIndex = 8;
-            TxbxTitulo.Text = "Vuelo";
-            TxbxTitulo.TextAlign = HorizontalAlignment.Center;
+            dateTimePicker2.Location = new Point(390, 237);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 64;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(434, 135);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(124, 23);
+            textBox8.TabIndex = 66;
+            textBox8.Text = "Horario de Apertura";
+            textBox8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(434, 208);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(124, 23);
+            textBox7.TabIndex = 67;
+            textBox7.Text = "Horario de Cierre";
+            textBox7.TextAlign = HorizontalAlignment.Center;
             // 
             // FrmPuertaEmbarque
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox7);
+            Controls.Add(textBox8);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(textBox6);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(TxbxTitulo);
             Controls.Add(button3);
             Controls.Add(button2);
@@ -98,10 +204,20 @@
 
         #endregion
 
+        private TextBox textBox6;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox TxbxTitulo;
         private Button button3;
         private Button button2;
         private Button button1;
         private Button BtEliminar;
-        private TextBox TxbxTitulo;
+        private DateTimePicker dateTimePicker2;
+        private TextBox textBox8;
+        private TextBox textBox7;
     }
 }
