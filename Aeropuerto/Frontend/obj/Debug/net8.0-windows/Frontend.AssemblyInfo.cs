d1768a574b4901d8e19bcc2c57398b786f8e823b
Nyxby2002 <nyxby2002@gmail.com>
2025-08-09 14:20:55 -0600

    Imagenes subidas

--- Aeropuerto/Frontend/obj/Debug/net8.0-windows/Frontend.AssemblyInfo.cs
+++ Aeropuerto/Frontend/obj/Debug/net8.0-windows/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e5b31cbafeb597a81866aab2a2abd5c31ad532")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
